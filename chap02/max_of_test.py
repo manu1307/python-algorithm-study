5450fb4e444cbd46d8f949b870b4cd30e6225acb
manu1307 <jasonsc@naver.com>
2023-07-30 10:19:20 +0900

    7/29 스터디 기록

---
+++ chap02/max_of_test.py
@@ -0,0 +1,9 @@
+from max import max_of
+
+t = (4,7,5.6,2,3.15,100)
+s= "string"
+a = ['DTS', 'AAC' , 'FlAC']
+
+print(f'{t}의 최댓값은 {max_of(t)}입니다.')
+print(f'{s}의 최댓값은 {max_of(s)}입니다.')
+print(f'{a}의 최댓값은 {max_of(a)}입니다.')
